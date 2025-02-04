881ce36f771c6969217695d073a207fb27545281
Shreyaschavan10 <166787514+Shreyaschavan10@users.noreply.github.com>
2025-02-04 19:04:48 +0530

    Update SQL_Script.sql

--- Customers who never order/SQL_Script.sql
+++ Customers who never order/SQL_Script.sql
@@ -2,3 +2,10 @@ SELECT c.name AS Customers
 FROM dbo.Customers c
 LEFT JOIN dbo.Orders o ON c.id = o.customerId
 WHERE o.customerId IS NULL;
+
+
+```tsql
+SELECT c.name AS Customers
+FROM Customers c
+LEFT JOIN Orders o ON c.id = o.customerId
+WHERE o.customerId IS NULL;
